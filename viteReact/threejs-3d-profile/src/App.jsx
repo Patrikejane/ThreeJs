a083247978e3ca04972643d03c15f65027fc257d
Sunimal Malkaklulage <sskmal@gmail.com>
2024-09-22 09:58:41 +0900

    3d js

---
+++ viteReact/threejs-3d-profile/src/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./component/sections/NavBar"
+
+const App = () => {
+  return (
+    <div className='max-w-7xl mx-auto'>
+        <Navbar />
+    </div>
+  )
+}
+export default App
---
+++ viteReact/threejs-3d-profile/src/component/sections/NavBar.jsx
@@ -0,0 +1,20 @@
+const Navbar = () => {
+  return (
+    <div className="text-white">
+      Navbar
+      
+      <header className="fixed top-0 left-0 right-0 z-50 
+      bg-black/90">
+        <div className="max-w-7xl mx-auto">
+          <div className="flex"></div>
+        </div>
+
+        
+      </header>
+    </div>
+
+
+  )
+}
+
+export default Navbar
